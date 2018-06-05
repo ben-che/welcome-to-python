63767ea213b9c03d0994527d76c514bc9c535f2c
benche <ben.che9@gmail.com>
2018-06-05 15:22:43 -0400

    added more questions

--- Module Two/index.py
+++ Module Two/index.py
@@ -1,12 +1,36 @@
 import math, random
-# Create a rock paper scissors game that takes in user input and computer input and prints whether the user wins or loses:
+################################################################################################################################################
+
+# Create a simple ATM system that either prints "Success" if the user has enough money for a withdraw, or "Error" if they do not have
+#       enough funds
+# Sample Tests:
+#   atmLogic(100) => "Success"
+#   atmLogic(10) => "Success"
+#   atmLogic(1000) => "Error"
+
+def atmLogic(withdrawAmount):
+    currentBalance = 100
+    # Define function below:
+
+################################################################################################################################################
+
+# Create a rock paper scissors game that takes in user input and computer input and prints whether the user wins, draws or loses:
+# Sample Tests:
+#   rockPaperScissors("rock","paper") => prints "You lose!"
+#   rockPaperScissors("paper","paper") => prints "Draw!"
+#   rockPaperScissors("scissors","paper") => prints "You win!"
+
+#   bonus: a statement that handles invalid input
+#   #   rockPaperScissors("herp","derp") => prints "Invalid input"
+
 def rockPaperScissors(userInput, computerInput):
     # Here, we run the computerPlays function, which will randomly generate "rock" "paper", or "scissors"
     print("Computer plays: " + computerInput)
     print("User plays:" + userInput)
 
     # Write your conditional flow below:
-        # Hint: Think about the win / lose / tie conditions
+        # Hint: Think about the win / lose / tie conditions and what gets printed
+
 
 # Code that generates computer input - you don't have to change anything here:
 def computerPlays():
@@ -20,7 +44,9 @@ def computerPlays():
         computerAnswer = "scissors"
     return computerAnswer
 
-# Call 
+# Tests: 
 rockPaperScissors("rock", computerPlays())
 rockPaperScissors("paper", computerPlays())
 rockPaperScissors("scissors", computerPlays())
+
+################################################################################################################################################
